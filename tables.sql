ddd5581d4906b7d23091a105e866e4e0e9e13d51
Nicolas <nicolas.tual@outlook.fr>
2024-03-24 22:06:25 +0100

    Fix: Add tsc-alias

--- tables.sql
+++ tables.sql
@@ -8,58 +8,14 @@ create table cards
 create index cards_id_index
     on cards (id);
 
-create table dishes
-(
-    id      int auto_increment
-        primary key,
-    card_id int         not null,
-    name    varchar(30) not null,
-    price   int         not null,
-    constraint dishes_cards_id_fk
-        foreign key (card_id) references cards (id)
-            on delete cascade
-);
-
-create index dishes_id_index
-    on dishes (id);
-
-create table menus
-(
-    id          int auto_increment
-        primary key,
-    card_id     int          not null,
-    name        varchar(30)  not null,
-    price       int          not null,
-    description varchar(150) not null,
-    constraint menus_cards_id_fk
-        foreign key (card_id) references cards (id)
-            on delete cascade
-);
-
-create index menus_id_index
-    on menus (id);
-
-create table menus_dishes
-(
-    menu_id  int not null,
-    dishe_id int not null,
-    constraint menus_dishes_dishes_id_fk
-        foreign key (dishe_id) references dishes (id)
-            on delete cascade,
-    constraint menus_dishes_menus_id_fk
-        foreign key (menu_id) references menus (id)
-            on delete cascade
-);
-
 create table users
 (
     id         int auto_increment
         primary key,
     email      varchar(512)                                   not null,
-    password   varchar(30)                                    not null,
+    password   varchar(72)                                    not null,
     name       varchar(255)                                   not null,
-    address    varchar(255)                                   not null,
-    phone      int                                            not null,
+    phone      varchar(10)                                    not null,
     role       enum ('CUSTOMER', 'DELIVERYMAN', 'RESTAURANT') not null,
     created_at datetime                                       not null,
     updated_at datetime                                       not null,
@@ -71,7 +27,8 @@ create table customers
 (
     id      int auto_increment
         primary key,
-    user_id int not null,
+    user_id int          not null,
+    address varchar(255) not null,
     constraint customers_users_id_fk
         foreign key (user_id) references users (id)
             on delete cascade
@@ -86,6 +43,7 @@ create table deliverymans
         primary key,
     user_id int                                              not null,
     status  enum ('AVAILABLE', 'UNAVAILABLE', 'ON_DELIVERY') not null,
+    address varchar(255)                                     not null,
     constraint deliverymans_users_id_fk
         foreign key (user_id) references users (id)
             on delete cascade
@@ -99,8 +57,9 @@ create table restaurants
     id          int auto_increment
         primary key,
     user_id     int          not null,
-    card_id     int          not null,
+    card_id     int          null,
     description varchar(150) not null,
+    address     varchar(255) not null,
     constraint restaurants_cards_id_fk
         foreign key (card_id) references cards (id)
             on delete cascade,
@@ -119,24 +78,75 @@ create table deliveries
     status         enum ('DELIVERED', 'PENDING', 'CANCELLED') not null,
     created_at     datetime                                   not null,
     constraint orders_customers_id_fk
-        foreign key (customer_id) references customers (id)
+        foreign key (customer_id) references customers (user_id)
             on delete cascade,
     constraint orders_deliverymans_id_fk
-        foreign key (deliveryman_id) references deliverymans (id)
+        foreign key (deliveryman_id) references deliverymans (user_id)
             on delete cascade,
     constraint orders_restaurants_id_fk
-        foreign key (restaurant_id) references restaurants (id)
+        foreign key (restaurant_id) references restaurants (user_id)
             on delete cascade
 );
 
 create index orders_id_index
     on deliveries (id);
 
+create table dishes
+(
+    id            int auto_increment
+        primary key,
+    card_id       int         not null,
+    restaurant_id int         not null,
+    name          varchar(30) not null,
+    price         int         not null,
+    constraint dishes_cards_id_fk
+        foreign key (card_id) references cards (id)
+            on delete cascade,
+    constraint dishes_restaurants_user_id_fk
+        foreign key (restaurant_id) references restaurants (user_id)
+            on delete cascade
+);
+
+create index dishes_id_index
+    on dishes (id);
+
+create table menus
+(
+    id            int auto_increment
+        primary key,
+    card_id       int          not null,
+    restaurant_id int          not null,
+    name          varchar(30)  not null,
+    price         int          not null,
+    description   varchar(150) not null,
+    constraint menus_cards_id_fk
+        foreign key (card_id) references cards (id)
+            on delete cascade,
+    constraint menus_restaurants_user_id_fk
+        foreign key (restaurant_id) references restaurants (user_id)
+            on delete cascade
+);
+
+create index menus_id_index
+    on menus (id);
+
+create table menus_dishes
+(
+    menu_id  int not null,
+    dishe_id int null,
+    constraint menus_dishes_dishes_id_fk
+        foreign key (dishe_id) references dishes (id)
+            on delete cascade,
+    constraint menus_dishes_menus_id_fk
+        foreign key (menu_id) references menus (id)
+            on delete cascade
+);
+
 create table orders_deliveries
 (
     delivery_id int not null,
-    menu_id     int not null,
-    dishe_id    int not null,
+    menu_id     int null,
+    dishe_id    int null,
     constraint orders_deliveries_deliveries_id_fk
         foreign key (delivery_id) references deliveries (id)
             on delete cascade,
@@ -153,3 +163,4 @@ create index restaurants_id_index
 
 create index users_id_index
     on users (id);
+
